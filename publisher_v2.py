420b8f5c710e40388dc5665b05d376dbdcb27bef
bbbuiseness <bbbuiseness@github.com>
2026-09-02 21:08:28 +0100

    Add editorial policy links and missing references

--- publisher_v2.py
+++ publisher_v2.py
@@ -739,6 +739,7 @@ def build_html(data, keyword_day, cover_filename, section_images=None):
         <ul>
           <li><a href="/about/">About Us</a></li>
           <li><a href="/contact/">Contact</a></li>
+          <li><a href="/editorial-policy/">Editorial Policy</a></li>
           <li><a href="/privacy/">Privacy Policy</a></li>
           <li><a href="/disclaimer/">Disclaimer</a></li>
           <li><a href="/terms/">Terms of Service</a></li>
